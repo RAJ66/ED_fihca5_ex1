8428dd2c371124994606fa086fd00ac0d5ce1bec
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-11-01 14:26:43 +0000

    interface ListADT e exceptions

---
+++ src/main/java/com/mycompany/ficha5_ex1/EmptyCollectionException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ficha5_ex1;
+
+/**
+ *
+ * @author vitor
+ */
+public class EmptyCollectionException extends Exception{
+
+    public EmptyCollectionException(String message) {
+        super(message);
+    }
+    
+}
