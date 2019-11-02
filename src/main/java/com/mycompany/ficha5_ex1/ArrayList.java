2260c83f7f75d9f0978474d9bf0423213b1958ff
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-11-02 14:20:08 +0000

    adicionado comentarios e melhoria no testes

--- src/main/java/com/mycompany/ficha5_ex1/ArrayList.java
+++ src/main/java/com/mycompany/ficha5_ex1/ArrayList.java
@@ -13,94 +13,160 @@ import java.util.Iterator;
  */
 public class ArrayList<T> implements ListADT<T> {
 
+     /**
+     *
+     * array of generic elements to represent the ArrayList
+     */
     protected T[] list;
+    /**
+     * int that represents the next available position in the ArrayList
+     */
     protected int rear;
-    protected static int DEFAULT_CAPACITY = 3;
+    /**
+     * constant to represent the default capacity of the ArrayList
+     */
+    protected static int DEFAULT_CAPACITY = 5;
 
+    /**
+     * Creates an empty list using the default capacity.
+     */
     public ArrayList() {
         this.list = (T[]) (new Object[DEFAULT_CAPACITY]);
         this.rear = 0;
     }
 
+    /**
+     * Creates an empty list using the specified capacity.
+     *
+     * @param tamanho represents the specified capacity
+     */
     public ArrayList(int tamanho) {
         this.list = (T[]) (new Object[tamanho]);
         this.rear = 0;
     }
 
-    //metodo so para testes
+    /**
+     * Used only for testing. Adds one element to the rear of this list.
+     *
+     * @param element the element to be added to the rear of this list
+     */
     public void add(T element) {
-        list[this.rear] = element;
-        this.rear++;
+        if (this.rear == this.list.length) {
+            this.expandCapacity();
+            list[this.rear] = element;
+            this.rear++;
+        } else {
+            list[this.rear] = element;
+            this.rear++;
+        }
     }
 
+    /**
+     * Used only for testing. Expands the capacity of this queue
+     */
+    public void expandCapacity() {
+        T[] newList = (T[]) (new Object[DEFAULT_CAPACITY * 2]);
+
+        for (int i = 0; i < this.rear; i++) {
+            newList[i] = this.list[i];
+        }
+
+        this.list = newList;
+    }
+
+    /**
+     * Removes and returns the first element from this list.
+     *
+     * @return the first element from this list
+     */
     @Override
     public T removeFirst() throws EmptyCollectionException {
         if (this.isEmpty()) {
             throw new EmptyCollectionException("Lista Vazia!");
-        }
-
-        //guarda item a ser removido
-        T removido = this.first();
-
-        for (int i = 0; i < this.size() - 1; i++) {
-            this.list[i] = this.list[i + 1];
-        }
-
-        //coloca o ultimo elemento a null
-        this.list[this.rear - 1] = null;
-
-        //decrementa rear
-        this.rear--;
-
-        //retorna item removido
-        return removido;
-    }
-
-    @Override
-    public T removeLast() throws EmptyCollectionException {
-        //se a lista estiver vazia
-        if (this.isEmpty()) {
-            throw new EmptyCollectionException("Lista Vazia!");
-        }
-        //guarda item a ser removido
-        T removido = this.last();
-
-        //coloca posicao desse item a null
-        this.list[this.rear - 1] = null;
-        //decrementa rear
-        this.rear--;
-        return removido;
-    }
-
-    @Override
-    public T remove(T element) throws EmptyCollectionException, ElementoNaoExisteException {
-        //se a lista estiver vazia
-        if (this.isEmpty()) {
-            throw new EmptyCollectionException("Lista Vazia!");
-        }
-        
-        int elementPosition = this.find(element);
-        
-        if (elementPosition == -1) {
-             throw new ElementoNaoExisteException("Elemento não existe!");
-        }
-        
-        //guarda o item a ser removido
-        T removido = this.list[elementPosition];
-        
-        
-         for (int i = elementPosition; i < this.rear - 1; i++) {
+        } else {
+            T removido = this.first();
+            for (int i = 0; i < this.rear - 1; i++) {
                 this.list[i] = this.list[i + 1];
             }
-         
-         //coloca posição desse item a null
-        this.list[this.rear - 1] = null;
-        
-        //decrementa rear
-        this.rear--;
-        
-         return removido;
+            this.list[this.rear - 1] = null;
+            this.rear--;
+            return removido;
+        }
+    }
 
+    /**
+     * Removes and returns the last element from this list.
+     *
+     * @return the last element from this list
+     * @throws ficha5edex1.EmptyCollectionException
+     */
+    @Override
+    public T removeLast() throws EmptyCollectionException {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Lista Vazia!");
+        } else {
+            T removido = this.last();
+            this.list[this.rear - 1] = null;
+            this.rear--;
+            return removido;
+        }
+    }
+
+    /**
+     * Removes and returns the specified element from this list.
+     *
+     * @param element the element to be removed from the list
+     * @return the removed element
+     * @throws ficha5edex1.EmptyCollectionException
+     * @throws ficha5edex1.ElementoNaoExisteException
+     */
+    @Override
+    public T remove(T element) throws EmptyCollectionException, ElementoNaoExisteException {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Lista Vazia!");
+        }
+
+        int elementPosition = this.find(element);
+
+        if (elementPosition == -1) {
+            throw new ElementoNaoExisteException("Elemento não existe!");
+        }
+        T removido = this.list[elementPosition];
+        for (int i = elementPosition; i < this.rear - 1; i++) {
+            this.list[i] = this.list[i + 1];
+        }
+        this.list[this.rear - 1] = null;
+        this.rear--;
+        return removido;
+    }
+
+    /**
+     * Returns a reference to the first element in this list.
+     *
+     * @return a reference to the first element in this list
+     * @throws ficha5edex1.EmptyCollectionException
+     */
+    @Override
+    public T first() throws EmptyCollectionException {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Lista Vazia!");
+        }
+        return this.list[0];
+
+    }
+
+    /**
+     * Returns a reference to the last element in this list.
+     *
+     * @return a reference to the last element in this list
+     * @throws ficha5edex1.EmptyCollectionException
+     */
+    @Override
+    public T last() throws EmptyCollectionException {
+        if (this.isEmpty()) {
+            throw new EmptyCollectionException("Lista Vazia!");
+        }
+        return this.list[this.rear - 1];
     }
 
     /**
@@ -118,22 +184,12 @@ public class ArrayList<T> implements ListADT<T> {
         return -1;
     }
 
-    @Override
-    public T first() throws EmptyCollectionException {
-        if (this.isEmpty()) {
-            throw new EmptyCollectionException("Lista Vazia!");
-        }
-        return this.list[0];
-    }
-
-    @Override
-    public T last() throws EmptyCollectionException {
-        if (this.isEmpty()) {
-            throw new EmptyCollectionException("Lista Vazia!");
-        }
-        return this.list[this.rear - 1];
-    }
-
+    /**
+     * Returns true if this list contains the specified target element.
+     *
+     * @param target the target that is being sought in the list
+     * @return true if the list contains this element
+     */
     @Override
     public boolean contains(T target) {
         if (this.find(target) != -1) {
@@ -142,18 +198,34 @@ public class ArrayList<T> implements ListADT<T> {
         return false;
     }
 
+    /**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if this list contains no elements
+     */
     @Override
     public boolean isEmpty() {
+        //se o rear for a primeira posição
         return this.rear == 0;
     }
 
+    /**
+     * Returns the number of elements in this list.
+     *
+     * @return the integer representation of number of elements in this list
+     */
     @Override
     public int size() {
         return this.rear;
     }
 
+    /**
+     * Returns an iterator for the elements in this list.
+     *
+     * @return an iterator over the elements in this list
+     */
     @Override
-    public Iterator<T> iterator() {
+    public Iterator iterator() {
         return new MyItr();
     }
 
@@ -173,9 +245,8 @@ public class ArrayList<T> implements ListADT<T> {
             str.append(this.list[this.rear - 1]);
             str.append("\n");
         }
-
         str.append("Nº elementos: ");
-        str.append(this.size());
+        str.append(this.rear);
         str.append("\n");
         str.append("Tamanho da lista: ");
         str.append(this.list.length);
@@ -185,29 +256,50 @@ public class ArrayList<T> implements ListADT<T> {
             str.append(this.list[i]);
             str.append("\n");
         }
-
         return str.toString();
     }
 
-    private class MyItr implements Iterator<T>{
+    /**
+     * Class that represents a Iterator
+     */
+    private class MyItr implements Iterator<T> {
+
+        /**
+         * int that represents the position of the iterator
+         */
         int cursor = 0;
-        
-        MyItr() {}
-        @Override
-        public boolean hasNext() {
-             return cursor != size();
+
+        /**
+         * Creates an Iterator.
+         *
+         */
+        MyItr() {
         }
 
+        /**
+         * Verifies if there is a element next to the current
+         *
+         * @return true if the element exists
+         */
+        @Override
+        public boolean hasNext() {
+            return cursor != size();
+        }
+
+        /**
+         * Moves the iterator to the next position.
+         *
+         * @return the previous position the cursor was
+         */
         @Override
         public T next() {
-             int i = cursor;
-                if (i>=size()) {
-                     System.out.println("Nao existe");
-                }
-                cursor++;
-                return list[i];
+            int i = cursor;
+            if (i >= size()) {
+                System.out.println("Nao existe");
+            }
+            cursor++;
+            return list[i];
         }
-        
-    } 
-   
+
+    }
 }
--- src/main/java/com/mycompany/ficha5_ex1/MainListADT.java
+++ src/main/java/com/mycompany/ficha5_ex1/MainListADT.java
@@ -22,118 +22,189 @@ public class MainListADT {
 
         ArrayList lista1 = new ArrayList();
 
-        int teste = 12;
+        int teste = 21;
 
         switch (teste) {
-            case 0: //teste removeFirst com array vazio
-
-                System.out.println(lista1.removeFirst());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 1: //teste removeFirst com array com 1 elemento
-                lista1.add(1);
-                System.out.println(lista1.toString());
-                System.out.println(lista1.removeFirst());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 2: //teste removeFirst com array com alguns
-                lista1.add(1);
-                lista1.add(2);
-
-                System.out.println(lista1.toString());
-                System.out.println(lista1.removeFirst());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 3: //teste removeFirst com array cheio
+            case 0: //teste removeFirst com elementos na lista
                 lista1.add(1);
                 lista1.add(2);
                 lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
 
                 System.out.println(lista1.toString());
+
+                System.out.println("Item removido: " + lista1.removeFirst() + "\n");
+
+                System.out.println(lista1.toString());
+                break;
+
+            case 1: //teste removeFirst sem elementos na lista
+                System.out.println(lista1.toString());
+                
                 System.out.println(lista1.removeFirst());
-                System.out.println(lista1.toString());
-
                 break;
-
-            case 4: //teste removeLast com array vazio
-
-                System.out.println(lista1.removeLast());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 5: //teste removeLast com array com 1 elemento
-                lista1.add(1);
-                System.out.println(lista1.toString());
-                System.out.println(lista1.removeLast());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 6: //teste removeLast com array com alguns
-                lista1.add(1);
-                lista1.add(2);
-
-                System.out.println(lista1.toString());
-                System.out.println(lista1.removeLast());
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 7: //teste removeLast com array cheio
+            
+            case 2: //teste removeFirst com alguns elementos na lista
                 lista1.add(1);
                 lista1.add(2);
                 lista1.add(3);
-
+                
                 System.out.println(lista1.toString());
-                System.out.println(lista1.removeLast());
+                
+                System.out.println("Item removido: " + lista1.removeFirst() + "\n");
+                
                 System.out.println(lista1.toString());
-
                 break;
-
-            case 8: //teste remove com um nao existente no array
+             
+            case 3: //teste removeFirst com 1 elemento na lista
                 lista1.add(1);
-                System.out.println(lista1.remove(2));
+                
                 System.out.println(lista1.toString());
-
+                System.out.println("Item removido: " + lista1.removeFirst() + "\n");
+                System.out.println(lista1.toString());
                 break;
-
-            case 9: //teste remove com array com 1 elemento
-                lista1.add(1);
-                System.out.println(lista1.toString());
-                System.out.println(lista1.remove(1));
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 10: //teste remove com array com alguns
-                lista1.add(1);
-                lista1.add(2);
-
-                System.out.println(lista1.toString());
-                System.out.println(lista1.remove(2));
-                System.out.println(lista1.toString());
-
-                break;
-
-            case 11: //teste remove com array cheio
+                
+            case 4: //teste removeLast com elementos na lista
                 lista1.add(1);
                 lista1.add(2);
                 lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
 
                 System.out.println(lista1.toString());
-                System.out.println(lista1.remove(3));
+                System.out.println("Item removido: " + lista1.removeLast() + "\n");
                 System.out.println(lista1.toString());
-
                 break;
 
-            case 12: //teste iterador
+            case 5: //teste removeLast sem elementos na lista
+                lista1.removeLast();
+                break;
+            
+            case 6: //teste removeLast com 1 elemento na lista
+                lista1.add(1);
+                System.out.println(lista1.toString());
+                System.out.println("Item removido: " + lista1.removeLast() + "\n");
+                System.out.println(lista1.toString());
+                break;
+                
+            case 7: //teste removeLast com alguns elementos na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                
+                System.out.println(lista1.toString());
+                System.out.println("Item removido: " + lista1.removeLast() + "\n");
+                System.out.println(lista1.toString());
+                break;
+
+            case 8: //teste contains de um elemento que existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+
+                System.out.println(lista1.contains(1));
+                break;
+
+            case 9: //teste contains de um elemento que não existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+
+                System.out.println(lista1.contains(0));
+                break;
+
+            case 10: //teste remove de elemento que existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
+
+                System.out.println(lista1.toString());
+                System.out.println("Item removido: " + lista1.remove(3) + "\n");
+                System.out.println(lista1.toString());
+                break;
+
+            case 11: //teste remove com lista vazia
+                lista1.remove(1);
+                break;
+
+            case 12: //teste remove de um elemento que nao existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                
+                lista1.remove(4);
+                break;
+                
+            case 13: //teste remove com 1 elemento na lista
+                lista1.add(1);
+                
+                System.out.println(lista1.toString());
+                System.out.println("Item removido: " + lista1.remove(1) + "\n");
+                System.out.println(lista1.toString());
+                break;
+                
+            case 14: //teste remove com alguns elementos na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                
+                System.out.println(lista1.toString());
+                System.out.println("Item removido: " + lista1.remove(2) + "\n");
+                System.out.println(lista1.toString());
+                break;
+                
+            case 15: //teste find de um elemento que existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
+                
+                System.out.println(lista1.find(3));
+                break;
+            
+            case 16: //teste find de um elemento que nao existe na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                
+                System.out.println(lista1.find(4));
+                break;
+                
+            case 17: //teste metodo first com elementos na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
+                
+                System.out.println(lista1.first());
+                break;
+                
+            case 18: //teste metodo first sem elementos na lista
+                System.out.println(lista1.first());
+                break;
+                
+            case 19: //teste metodo last com elementos na lista
+                lista1.add(1);
+                lista1.add(2);
+                lista1.add(3);
+                lista1.add(4);
+                lista1.add(5);
+                
+                System.out.println(lista1.last());
+                break;
+                
+            case 20: //teste metodo last sem elementos na lista
+                System.out.println(lista1.last());
+                break;
+                
+            case 21: //teste iterador
                 lista1.add(1);
                 lista1.add(2);
                 lista1.add(3);
